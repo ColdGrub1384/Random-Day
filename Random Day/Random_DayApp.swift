9da82744965d36a1ecd20685ae6e5c81173777bc
Adrian Labbé <adrilabbelol@gmail.com>
2021-04-23 20:33:58 -0400

    Initial Commit

---
+++ Random Day/Random_DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_DayApp.swift
+//  Random Day
+//
+//  Created by Emma Labbé on 23-04-21.
+//
+
+import SwiftUI
+
+@main
+struct Random_DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
